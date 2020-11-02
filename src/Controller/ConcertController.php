1e82e78821f1c797a89dd3c09ddced7a46ff1b32
LaureCh <laure.christol@gmail.com>
2020-11-02 23:42:21 +0100

    TP1-ControleursVues

---
+++ src/Controller/ConcertController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConcertController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        return $this->render('concert/index.html.twig', [
+            'controller_name' => 'ConcertController',
+        ]);
+    }
+
+
+    /**
+     * Affiche une liste de concerts
+     *
+     * @param string $name
+     * @return Response
+     *
+     * @Route("/concert/{name}", name="list")
+     */
+    public function list(string $name): Response
+    {
+        return $this->render('concert/list.html.twig', [
+            'name' => $name
+        ]);
+    }
+}
