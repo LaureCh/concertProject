1ad2f97fc2295fff548d3d30fdde636bdb45e02d
LaureCh <laure.christol@gmail.com>
2020-12-06 21:19:07 +0100

    TP4-Repository

--- src/Controller/ConcertController.php
+++ src/Controller/ConcertController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Show;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ConcertController extends AbstractController
+class ShowController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
@@ -25,13 +26,15 @@ class ConcertController extends AbstractController
      * @param string $name
      * @return Response
      *
-     * @Route("/concert/{name}", name="list")
+     * @Route("/show/{name}", name="list")
      */
     public function list(string $name): Response
     {
-        return $this->render('concert/list.html.twig', [
+        $repository = $this->getDoctrine()->getRepository(Show::class);
+
+        return $this->render('show/list.html.twig', [
             'name' => $name,
-            'concerts' => ['Dionysos', 'Chapelier Fou']
+            'concerts' => $repository->findAll()
             ]
         );
     }
